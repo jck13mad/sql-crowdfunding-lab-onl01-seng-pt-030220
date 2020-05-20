b15b4597c257b518a5e9305d0d14abe3614dbe62
Jack Madden <jck13mad@gmail.com>
2020-05-20 18:19:05 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -29,7 +29,7 @@ end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_summed_amount
   "SELECT users.name, SUM(pledges.amount) AS total_pledges FROM users
-  INNER JOIN "
+  INNER JOIN pledges ON "
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
